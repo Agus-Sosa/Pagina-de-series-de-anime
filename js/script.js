866333a4c786c2a4ecf0d3d4cc3279eca04a3ab3
Agus-Sosa <sosaagustin351@gmail.com>
2022-09-22 04:13:55 -0300

    Pagina casi termina con javascript

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let Suscripcion1 = 'Basica'
+let precioSubscripcion = 10
+
+let Suscripcion2 = 'Estandar'
+let precioSubscripcion2 = 15.99
+
+let Suscripcion3 = 'Premium'
+let precioSubscripcion3 = 19.99
+
+let ImpuestosDolar = 1.75 
+let PrecioDolar = 150
+
+function precio(precio, precioDolar, impuestos) {
+    preciototal = (precio * precioDolar) * impuestos 
+}
+
+alert('Estas son las suscripciones:\n - Basica\n - Estandar\n - Premium\n')
+
+let Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC')
+
+while(Respuesta.toUpperCase() != 'ESC') {
+
+    if(Respuesta == 'Basica') {
+    
+    precio(precioSubscripcion, PrecioDolar, ImpuestosDolar)
+    alert('Precio total con impuestos inlcuidos: ' + preciototal + ' ARS')
+        
+    let confirmar =  prompt('Desea confirmar la suscripcion? \n - SI\n - NO ')
+
+    if (confirmar == 'SI') {
+        alert('Compra Relizada')
+    }
+        
+    break
+    }
+    
+
+
+    else if(Respuesta == 'Estandar') {
+
+        precio(precioSubscripcion2, precioDolar, ImpuestosDolar)
+        alert('Precio total con impuesto incluidos: ' + preciototal + ' ARS')
+
+        let confirmar = ('Desea confirmar la suscripcion? \n - SI\n - NO')
+
+        if(confirmar == 'SI') {
+            alert('Compra realizada')
+        }
+        break
+
+    }
+
+    else if(Respuesta == 'Premium') {
+        
+        precio(precioSubscripcion3, PrecioDolar, ImpuestosDolar)
+        alert('Precio total con impuestos inlcuidos ' + preciototal)
+
+        let confirmar = prompt('Desea confirmar la suscripcion? \n - SI\n - NO')
+        
+        if(confirmar.toUpperCase() == 'SI') {
+            alert('Compra realizada')
+        }
+
+        break
+    }
+
+
+
+}
+
+
+
+function RespuestaNo() {}
