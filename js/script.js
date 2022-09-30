ff5494bcbc988c3e6a664ef522bf4d0f2786d709
Agus-Sosa <sosaagustin351@gmail.com>
2022-09-30 17:00:43 -0300

    actualizacion

--- js/script.js
+++ js/script.js
@@ -1,63 +1,75 @@
-let Suscripcion1 = 'Basica'
-let precioSubscripcion = 10
 
-let Suscripcion2 = 'Estandar'
-let precioSubscripcion2 = 15.99
+let Suscripcion1 = {
+    nombre: 'Basica',
+    precio: 10,
+}
 
-let Suscripcion3 = 'Premium'
-let precioSubscripcion3 = 19.99
+let Suscripcion2 = {
+    
+    nombre: 'Estandar',
+    precio: 15,
+
+}
+
+let Suscripcion3 = {
+
+    nombre: 'Premium',
+    precio:  19.99,
+}
+
+let listaSuscripciones = [Suscripcion1.nombre, Suscripcion2.nombre, Suscripcion3.nombre]
+
 
 let ImpuestosDolar = 1.75 
 let PrecioDolar = 150
 
 function precio(precio, precioDolar, impuestos) {
-    preciototal = (precio * precioDolar) * impuestos 
+    preciototal = Math.trunc(precio * precioDolar) * impuestos 
 }
 
-alert('Estas son las suscripciones:\n - Basica\n - Estandar\n - Premium\n')
+alert('Estas son las suscripciones: \n - ' + listaSuscripciones.join('\n - '))
 
-let Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC')
+let Respuesta = prompt('Seleccion que suscripcion desea acceder o presione ESC: \n  - ' + listaSuscripciones.join('\n - ') + ' \n - ESC').toUpperCase()
 
 while(Respuesta.toUpperCase() != 'ESC') {
 
-    if(Respuesta == 'Basica') {
-    
-    precio(precioSubscripcion, PrecioDolar, ImpuestosDolar)
+    if(Respuesta == 'BASICA') {
+    precio(Suscripcion1.precio, PrecioDolar, ImpuestosDolar)
     alert('Precio total con impuestos inlcuidos: ' + preciototal + ' ARS')
         
     let confirmar =  prompt('Desea confirmar la suscripcion? \n - SI\n - NO ')
 
-    if (confirmar == 'SI') {
+    if (confirmar.toUpperCase() == 'SI') {
         alert('Compra Relizada')
     }
 
-    Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC')
+    Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC').toUpperCase()
 
     // break
     }
     
 
 
-    else if(Respuesta == 'Estandar') {
+    else if(Respuesta == 'ESTANDAR') {
 
-        precio(precioSubscripcion2, PrecioDolar, ImpuestosDolar)
+        precio(Suscripcion2.precio, PrecioDolar, ImpuestosDolar)
         alert('Precio total con impuesto incluidos: ' + preciototal + ' ARS')
 
         let confirmar = prompt('Desea confirmar la suscripcion? \n - SI\n - NO')
 
-        if(confirmar.toLocaleUpperCase() == 'SI') {
+        if(confirmar.toUpperCase() == 'SI') {
             alert('Compra realizada')
         }
 
-        Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC')
+        Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC').toUpperCase()
 
         // break
 
     }
 
-    else if(Respuesta == 'Premium') {
+    else if(Respuesta == 'PREMIUM') {
         
-        precio(precioSubscripcion3, PrecioDolar, ImpuestosDolar)
+        precio(Suscripcion3.precio, PrecioDolar, ImpuestosDolar)
         alert('Precio total con impuestos inlcuidos ' + preciototal)
 
         let confirmar = prompt('Desea confirmar la suscripcion? \n - SI\n - NO')
@@ -68,7 +80,7 @@ while(Respuesta.toUpperCase() != 'ESC') {
             
         }
         
-        Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC')
+        Respuesta = prompt('Seleccion que subscripcion desea acceder o presione ESC: \n - Basica - 9.99\n - Estandar - 15.99\n - Premium - 19.99\n - ESC').toUpperCase()
 
         // break
 
